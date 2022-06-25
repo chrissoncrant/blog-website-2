569b5cc6dce0586e93033f76aef77e178bd60ae8
Chris Soncrant <christopher.soncrant@gmail.com>
2022-06-24 20:30:43 -0700

    created some basic styling

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 const articles = [
     {
         title: "First Blog Post", 
-        date: new Date('June 24, 2022'),
+        date: new Date(Date.now()).toString(),
         description: 'Just testing things out yo.',
         body: 'Lots of writing goes here because this is what it is all about. \r\rWriting writing writing on the wall of the cafeteria with the gravy from the mashed potatoes that I didn\'t pour on becuase I like \' em plain. \r\rWhat I wrote isn\'t so important, though it did change a few people\'s lives. \r\rI know this because they told me: "Dude, your gravy written message changed me." And I was like "Cool, man or honey (depending on who told me)." \r\rAnd that\'s the end of this crazy story.'
     }
